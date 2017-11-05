b78714a6e982b53ee45400fb0a6d19c7357fd3b2
Aria Moraine <ariamoraine@gmail.com>
2017-11-05 11:40:30 -0500

    styling looking ok

--- client/reactThings/components/add.js
+++ client/reactThings/components/add.js
@@ -6,170 +6,97 @@ export default class Add extends Component {
     super(props)
 
     this.state = {
-      formInfo: {
-        Neck: {
-           fullROM: {
-             title: 'Full ROM',
-             value: null
-           },
-           tenderParaspinalMuscles: {
-             title: 'Tenderness of paraspinal muscles',
-             value: null
-           },
-           tenderCervical: {
-             title: 'Midline cervucak tenderness',
-             vlaue: null
-           },
-           notes: {
-             title: 'Neck notes',
-             value: ''
-           }
-          },
-          MS: {
-           aao: {
-             title: 'Awake, alert, oriented',
-             value: null
-           },
-           language: {
-             title: 'Normal language/responsiveness',
-             value: null
-           },
-           memory: {
-             title: 'Normal memory and concentration',
-             value: null
-           },
-           notes: {
-             title: 'MS notes',
-             value: ''
-           }
-          },
-          'Cranial Nerves': {
-           face: {
-             title: 'Face symmetric',
-             value: null
-           },
-           palate: {
-             title: 'Palate symmetric',
-             value: null
-           },
-           tongue: {
-             title: 'Tongue movement symmetric',
-             value: null
-           },
-           shoulder: {
-             title: 'Shoulder shrug normal',
-             value: null
-           },
-           notes: {
-             title: 'Cranial notes',
-             value: ''
-           }
-          },
-          Eyes: {
-           pupils: {
-             title: 'Pupils equal, round, reactive to light',
-             value: null
-           },
-           visualField: {
-             title: 'Visual fields full',
-             value: null
-           },
-           smoothPursuit: {
-             title: 'Smooth Pursuit normal',
-             value: null
-           },
-           succades: {
-             title: 'Succades normal',
-             value: null
-           },
-           convergence: {
-             title: 'Convergence',
-             value: null
-           },
-           vor: {
-             title: 'VOR normal',
-             value: null
-           },
-           vorSuppression: {
-             title: 'VOR suppression normal',
-             value: null
-           },
-           notes: {
-             title: 'Eye Notes',
-             value: ''
-           }
-          },
-          Motor: {
-           strength: {
-             title: 'Normal proximal/distance strength',
-             value: null
-           },
-           fingerNose: {
-             title: 'Finger to nose',
-             value: null
-           },
-           notes: {
-             title: 'Motor Notes',
-             value: ''
-           }
-          },
-          sensory: {
-           touch: {
-             title: 'Normal sensation to light touch',
-             value: null
-           },
-           notes: {
-             title: 'Sensory notes',
-             value: ''
-           }
-          },
-          Proprioception: {
-           romberg: {
-             title: 'Romberg normal',
-             value: null
-           },
-           fakuda: {
-             title: 'Fakuda normal',
-             value: null
-           },
-           notes: {
-             title: 'Proprioception notes',
-             value: ''
-           }
-          },
-          Balance: {
-           testOne: {
-             title: 'Normal single leg, eyes closed, squat',
-             value: null
-           },
-           testTwo: {
-             title: 'Normal tandem forward/backward',
-             value: null
-           }
-          }
-       }
+      Neck: {
+       'Full ROM': false,
+       'Tenderness of paraspinal muscles': false,
+       'Midline cervucak tenderness': false,
+       'notes': ''
+      },
+      MS: {
+        'Awake, alert, oriented': false,
+        'Normal language/responsiveness': false,
+        'Normal memory and concentration': false,
+        'notes': ''
+      },
+      'Cranial Nerves': {
+       'Face symmetric': false,
+       'Palate symmetric': false,
+       'Tongue movement symmetric': false,
+       'Shoulder shrug normal': false,
+       'notes': ''
+      },
+      Eyes: {
+        'Pupils equal, round, reactive to light': false,
+        'Visual fields full': false,
+        'Smooth Pursuit normal': false,
+        'Succades normal': false,
+        'Convergence': false,
+        'VOR normal': false,
+        'VOR suppression normal': false,
+        'notes': ''
+      },
+      Motor: {
+       'Normal proximal/distance strength': false,
+       'Finger to nose': false,
+       'notes': ''
+      },
+      Sensory: {
+       'Normal sensation to light touch': false,
+       'notes': ''
+      },
+      Proprioception: {
+       'Romberg normal': false,
+       'Fakuda normal': false,
+       'notes': ""
+      },
+      Balance: {
+       'Normal single leg, eyes closed, squat': false,
+       'Normal tandem forward/backward': false,
+       'notes': ""
+      }
     }
   }
 
+  click (event, thing) {
+    // console.log('inside click', event, thing)
+    // console.log(this.state.formInfo)
+    // console.log("test", this.state[event])
+    let test = {
+      [thing]: !this.state[event][thing]
+    }
+
+    let nextTest = Object.assign(this.state[event], test)
+    this.setState({
+      [event]: nextTest
+    })
+  }
+
   render () {
+    console.log('state', this.state)
     return (
       <div>
+
       <div className='title'>Add page</div>
-      {Object.keys(this.state.formInfo).map(element => {
-        console.log(element)
+      {Object.keys(this.state).map((element, idx) => {
+        // console.log(element)
         return (
-          <div>
-          <div className='cat'>{element}</div>
-          {Object.keys(this.state.formInfo[element]).map(subCat => {
-            console.log(subCat)
+          <div key={idx}>
+          <span className='cat'>{element}</span>
+          {Object.keys(this.state[element]).map((subCat, idx) => {
             return(
-              <div>
-                <div>{this.state.formInfo[element][subCat].title}</div>
-                <input
-                  style={{display: 'inline'}}
-                  type="checkbox"
-                  name={this.state.formInfo[element][subCat]}
-                  value={this.state.formInfo[element][subCat].value || false} />
+              <div className="wrapper" key={idx}>
+                <div>{subCat}</div>
+                {
+                  subCat === 'notes' ?
+                    // <div>NOTE SECTION</div>
+                    <div>
+                    <textarea className="textarea" type="textarea" placeholder={`${element} notes`}></textarea>
+                    </div>
+                  :
+                    <div className="switch">
+                    <div onClick={() => this.click(element, subCat)} className={`${this.state[element][subCat]}`}>{this.state[element][subCat] ? 'Yes' : 'No'}</div>
+                    </div>
+                }
               </div>
             )
           })
